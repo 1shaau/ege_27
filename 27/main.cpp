3f34f0ef2975c6da7590e0dbe57ddbb5ccf71e19
vlad <shuryginvlad6@gmail.com>
2021-11-09 11:32:35 +0300

    checked divideability by 55

--- 27/main.cpp
+++ 27/main.cpp
@@ -9,7 +9,7 @@ void number_of_products_div() /// number of products dividable by 55
      /**
     1. считать и запомнить все числа +
     2. вывести все пары чисел +
-    3. вместо пар вывести произведения
+    3. вместо пар вывести  +
     4. проверить делимость
     5. посчитать и вывести кол-во подходящих произведений
     */
@@ -21,7 +21,11 @@ void number_of_products_div() /// number of products dividable by 55
 
     for(int j(0); j<n; j++)
         for (int i(j+1); i<n; i++)
-            cout << numbers[j]*numbers[i] << endl;
+        {
+            int cur_prod(numbers[j]*numbers[i]);
+            if (cur_prod%55 == 0)
+                cout << cur_prod << endl;
+        }
 }
 
 int main()
