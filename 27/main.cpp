ba99fa537624a808cbca4b9d2628635126fb73c6
vlad <shuryginvlad6@gmail.com>
2021-11-09 12:15:44 +0300

    reads numbers

--- 27/main.cpp
+++ 27/main.cpp
@@ -12,6 +12,14 @@ void number_of_products_div() /// number of products dividable by 55
   3. ответ n55*n11+n55*n5+n55*no+n55*(n55-1)/2*n5*n11
   */
 
+  int n(0), cur(0);
+  int n55(0), n11(0), n5(0), no(0);
+  cin >> n;
+  for (int i(0); i<n; i++)
+  {
+     cin >> cur;
+     cout << '\t' << cur << endl;
+  }
 }
 
 int main()
