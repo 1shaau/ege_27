3935974ff9dc36a680d023735c9d6637105f1991
vlad <shuryginvlad6@gmail.com>
2021-11-09 10:47:27 +0300

    added development plan

--- 27/main.cpp
+++ 27/main.cpp
@@ -6,6 +6,13 @@ using namespace std;
 void number_of_products_div() /// number of products dividable by 55
 {
     cout << "Hello world!" << endl;
+     /**
+    1. считать и запомнить все числа
+    2. вывести все пары чисел
+    3. вместо пар вывести произведения
+    4. проверить делимость
+    5. посчитать и вывести кол-во подходящих произведений
+    */
 }
 
 int main()
