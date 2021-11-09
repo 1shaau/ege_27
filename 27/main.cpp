3f6f778a7d2872e81292e941e22a1b10d0a8f5c1
vlad <shuryginvlad6@gmail.com>
2021-11-09 11:27:38 +0300

    shows all pairs

--- 27/main.cpp
+++ 27/main.cpp
@@ -17,10 +17,11 @@ void number_of_products_div() /// number of products dividable by 55
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i<n; i++)
-       cin >> numbers[i];
-    for (int i(0); i<n; i++)
-       cout << numbers[i] << ' ';
+        cin >> numbers[i];
 
+    for(int j(0); j<n; j++)
+        for (int i(j+1); i<n; i++)
+            cout << numbers[j] << ' ' << numbers[i] << endl;
 }
 
 int main()
