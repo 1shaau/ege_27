1de746d10f747f210bbffce60f2536df979958e8
vlad <shuryginvlad6@gmail.com>
2021-11-09 11:00:16 +0300

    reads and shows numbers

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 
 void number_of_products_div() /// number of products dividable by 55
 {
-    cout << "Hello world!" << endl;
      /**
-    1. считать и запомнить все числа
+    1. считать и запомнить все числа +
     2. вывести все пары чисел
     3. вместо пар вывести произведения
     4. проверить делимость
     5. посчитать и вывести кол-во подходящих произведений
     */
+    int n(0);
+    cin >> n;
+    vector<int> numbers(n, 0);
+    for (int i(0); i<n; i++)
+       cin >> numbers[i];
+    for (int i(0); i<n; i++)
+       cout << numbers[i] << ' ';
+
 }
 
 int main()
