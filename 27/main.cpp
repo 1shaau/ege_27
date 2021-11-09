e634318bf6d541cfc143efd8c16a7d318edcd915
vlad <shuryginvlad6@gmail.com>
2021-11-09 11:59:06 +0300

    added development plan

--- 27/main.cpp
+++ 27/main.cpp
@@ -5,7 +5,13 @@ using namespace std;
 
 void number_of_products_div() /// number of products dividable by 55
 {
-    cout << "Hello world!" << endl;
+  /**
+  1. считать число
+  2. добавить его к одному его из счетчиков соответствующего класса эквивалентности:
+     делится на 55, делится на 5 не на 11, делится на 11 не на 5, не делится ни на что
+  3. ответ n55*n11+n55*n5+n55*no+n55*(n55-1)/2*n5*n11
+  */
+
 }
 
 int main()
