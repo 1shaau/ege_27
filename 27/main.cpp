17bcc9e776fb0e7250ff72b17ccac32c794507d0
vlad <shuryginvlad6@gmail.com>
2021-11-09 10:46:27 +0300

    added function

--- 27/main.cpp
+++ 27/main.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 
-int main()
+
+void number_of_products_div() /// number of products dividable by 55
 {
     cout << "Hello world!" << endl;
+}
+
+int main()
+{
+    number_of_products_div();
     return 0;
 }
