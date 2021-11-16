7eaef6b9258b2edd117811657005a5edad5463ae
vlad <shuryginvlad6@gmail.com>
2021-11-16 10:46:51 +0300

    reads and shows all numbers

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void max_sum_div()
 {
     /**
-    1. считать и сохранить все числа
+    1. считать и сохранить все числа +
     2. вывести все пары
     3. вместо пары вывести суммы
     4. проверить делмость суммы
     5. вывести только наибольшую сумму
     */
+  int n(0);
+  cin >> n;
+  vector<int> numbers(n);
+  for (int i(0); i<n; i++)
+    cin >> numbers[i];
+  cout << endl;
+  for (int i(0); i<n; i++)
+    cout << numbers[i] << ' ';
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div();
     return 0;
 }
