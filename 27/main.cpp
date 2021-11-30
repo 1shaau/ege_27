b6a670693b5730e9ed9332aa8fadc43611bf9879
vlad <shuryginvlad6@gmail.com>
2021-11-30 10:47:27 +0300

    reads and shows all numbers

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+void min_sum_distance()
+{
+    /**
+    1. считать числа
+    2. вывести пары
+    3. вывести пары с минимальным расстоянием
+    4. вместо вывода пары посчитать сумму
+    5. найти минимум среди сумм
+    **/
+  int n(0);
+  cin >> n;
+  vector<int> numbers(n);
+  for (int i(0); i<n; i++)
+  cin >> numbers[i];
+  cout << endl;
+  for (int i(0); i<n; i++)
+    cout << numbers[i] << ' ';
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    min_sum_distance();
     return 0;
 }
