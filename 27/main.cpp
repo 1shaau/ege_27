2d09f6d4475a2ccdb68aba12c15aa5ba99bb98ed
vlad <shuryginvlad6@gmail.com>
2021-11-30 10:48:33 +0300

    shows all pairs

--- 27/main.cpp
+++ 27/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 void min_sum_distance()
 {
     /**
-    1. считать числа
-    2. вывести пары
+    1. считать числа +
+    2. вывести пары +
     3. вывести пары с минимальным расстоянием
     4. вместо вывода пары посчитать сумму
     5. найти минимум среди сумм
@@ -15,10 +15,10 @@ void min_sum_distance()
   cin >> n;
   vector<int> numbers(n);
   for (int i(0); i<n; i++)
-  cin >> numbers[i];
-  cout << endl;
-  for (int i(0); i<n; i++)
-    cout << numbers[i] << ' ';
+    cin >> numbers[i];
+  for(int j(0); j<n; j++)
+    for (int i(j+1); i<n; i++)
+        cout << numbers[j] << ' ' << numbers[i] << endl;
 }
 
 int main()
