108c81e355925aab596a6b8dffdeceb79af48a58
vlad <shuryginvlad6@gmail.com>
2021-12-21 10:56:27 +0300

    shows all triples

--- 27/main.cpp
+++ 27/main.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 void A()
 {
-    cout << "Hello world!" << endl;
     /**
     1. Считать все числа +
     2. Вывести их на экран +
-    3. Вывести все пары
-    4. Вывести все тройки
+    3. Вывести все пары +
+    4. Вывести все тройки +
     5. проверить тройки чисел на условие
     6. Вывести только подходящие тройки
     **/
@@ -19,9 +18,10 @@ void A()
     vector<int> numbers(n);
     for (int i(0); i<n; i++)
         cin >> numbers[i];
-    for(int j(0); j<n; j++)
-        for (int i(j+1); i<n; i++)
-            cout << numbers[j] << ' ' << numbers[i] << endl;
+        for (int j(0); j<n; j++)
+            for (int k(0); k<n; k++)
+                for (int i(k+j); i<n; i++)
+                    cout << numbers[k] << ' ' << numbers[j] << ' ' << numbers[i] << endl;
 
 }
 
