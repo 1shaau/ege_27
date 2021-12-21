86ff24e931d2e42bae2bba2ffab2d4c2d20b2d91
vlad <shuryginvlad6@gmail.com>
2021-12-21 10:40:31 +0300

    reads and shows all numbers

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,13 +7,22 @@ void A()
 {
     cout << "Hello world!" << endl;
     /**
-    1. Считать все числа
-    2. Вывести их на экран
+    1. Считать все числа +
+    2. Вывести их на экран +
     3. Вывести все пары
     4. Вывести все тройки
     5. проверить тройки чисел на условие
     6. Вывести только подходящие тройки
     **/
+    int n(0);
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i(0); i<n; i++)
+        cin >> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << ' ';
+
 }
 
 
