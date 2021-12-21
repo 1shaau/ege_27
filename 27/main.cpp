3a3bfffdc9347054508c05c77e5bda6f226c4c04
vlad <shuryginvlad6@gmail.com>
2021-12-21 11:14:00 +0300

    checked divide ability by 4 and printed max triple

--- 27/main.cpp
+++ 27/main.cpp
@@ -12,19 +12,26 @@ void A()
     4. Вывести все тройки +
     5. проверить тройки чисел на условие
     6. Вывести только подходящие тройки
+    7. Вывести максимальную тройку в виде суммы
     **/
     int n(0);
     cin >> n;
     vector<int> numbers(n);
+    int max_sum(0);
     for (int i(0); i<n; i++)
         cin >> numbers[i];
         for (int j(0); j<n; j++)
+
             for (int k(0); k<n; k++)
-                for (int i(k+j); i<n; i++)
-                    cout << numbers[k] << ' ' << numbers[j] << ' ' << numbers[i] << endl;
+                for (int i(k+j+1); i<n; i++)
+                   {
+                    int cur_sum(numbers[j]+numbers[i] + numbers[k]);
+                        if (cur_sum%4 != 0 and cur_sum > max_sum)
+                            max_sum = cur_sum;
+                   }
+                        cout << max_sum << endl;
 
 }
-
 
 int main()
 {
